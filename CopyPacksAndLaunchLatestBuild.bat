284442a33506b410bb8819287911f73a14b6d43b
Peter Johnson <peterandersjohnson@outlook.com>
2026-01-14 23:14:27 -0600

    First update to support rendering metrics

--- CopyPacksAndLaunchLatestBuild.bat
+++ CopyPacksAndLaunchLatestBuild.bat
@@ -4,4 +4,4 @@ set JavaHome="C:\JAVA\jdk-23\bin\java"
 cd "%~dp0build\libs"
 
 ::%JavaHome% --add-opens=javafx.graphics/javafx.scene=ALL-UNNAMED --add-opens=jdk.unsupported/sun.misc=ALL-UNNAMED -jar BiomeTool-0.4.9-win.jar
-%JavaHome% -jar BiomeTool-0.4.9-win.jar
+%JavaHome% -jar BiomeToolEnhanced-0.5.1-all.jar
