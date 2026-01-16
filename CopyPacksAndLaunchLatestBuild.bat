d370624c4f120845b86133cff8038f1036f283b6
Peter Johnson <peterandersjohnson@outlook.com>
2026-01-16 15:28:35 -0600

    Add copying from another git location for iteration and testing

--- CopyPacksAndLaunchLatestBuild.bat
+++ CopyPacksAndLaunchLatestBuild.bat
@@ -1,3 +1,42 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Define source and destination folders
+set "SOURCE=C:\Projects\ORIGEN2"
+set "DEST=C:\Projects\BiomeTool\build\libs\packs\terra-origen"
+
+echo Starting copy operation...
+echo Source: %SOURCE%
+echo Destination: %DEST%
+echo.
+
+:: Copy everything recursively, excluding specified folders
+:: Note: For wildcard exclusions, just use the pattern without full path
+robocopy "%SOURCE%" "%DEST%" /E ^
+    /XD ".*" "_*" "OldPromptAndReviewReferences" "Review" ^
+    /R:3 /W:5
+
+:: Delete all files from destination root EXCEPT the three we want
+echo.
+echo Cleaning up root files...
+for %%F in ("%DEST%\*.*") do (
+    set "filename=%%~nxF"
+    if /i not "!filename!"=="pack.yml" (
+        if /i not "!filename!"=="meta.yml" (
+            if /i not "!filename!"=="customization.yml" (
+                echo Deleting: !filename!
+                del "%%F"
+            )
+        )
+    )
+)
+
+echo.
+echo Copy operation completed.
+echo.
+
+endlocal
+
 SET "scriptPath=%~dp0"
 set JavaHome="C:\JAVA\jdk-23\bin\java"
 
