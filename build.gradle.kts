c753b4b34655fbf3dc9aa8e4f03d1d8452746fb3
Peter Johnson <peterandersjohnson@outlook.com>
2026-04-12 13:40:18 -0500

    Terra version correction

--- build.gradle.kts
+++ build.gradle.kts
@@ -125,7 +125,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
 
-    val terraGitHash = "4ae11d8ca"
+    val terraGitHash = "c3538321f"
 
     bootstrapTerraAddon("com.dfsek.terra:api-addon-loader:0.1.0-BETA-$terraGitHash")
     bootstrapTerraAddon("com.dfsek.terra:manifest-addon-loader:1.0.0-BETA-$terraGitHash")
