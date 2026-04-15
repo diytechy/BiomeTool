03ce0a42c7974cc255655cfb696ed4ce37122b22
Peter Johnson <peterandersjohnson@outlook.com>
2026-04-15 12:30:44 -0500

    Change to copy, clear region data on MC for quick recheck over same biomes

--- CopyPacks.bat
+++ CopyPacks.bat
@@ -12,7 +12,7 @@ echo.
 
 :: Copy everything recursively, excluding specified folders
 robocopy "%SOURCE%" "%DEST%" /E ^
-    /XD ".*" "_*" "OldPromptAndReviewReferences" "Review" ^
+    /XD ".*" "_*" "OldPromptAndReviewReferences" "Review" "build" "gradlew" "memory" ^
     /R:3 /W:5 /XO /NDL /NJH /NJS /purge
 
 :: Delete all files from destination root EXCEPT the three we want
@@ -39,7 +39,7 @@ echo.
 
 :: Copy everything recursively, excluding specified folders
 robocopy "%SOURCE%" "%DEST%" /E ^
-    /XD ".*" "_*" "OldPromptAndReviewReferences" "Review" ^
+    /XD ".*" "_*" "OldPromptAndReviewReferences" "Review" "build" "gradlew" "memory" ^
     /R:3 /W:5 /XO /NDL /NJH /NJS /purge
 
 
@@ -48,8 +48,18 @@ echo Compressing CHIMERA folder...
 set "CHIMERA_DIR=C:\MC\MINECRAFT_SERVER_TMP_26-1\plugins\Terra\packs\CHIMERA"
 set "CHIMERA_ZIP=C:\MC\MINECRAFT_SERVER_TMP_26-1\plugins\Terra\packs\CHIMERA.zip"
 if exist "%CHIMERA_ZIP%" del "%CHIMERA_ZIP%"
-powershell -NoProfile -Command "Compress-Archive -Path '%CHIMERA_DIR%\*' -DestinationPath '%CHIMERA_ZIP%'"
+powershell -NoProfile -Command "Add-Type -Assembly System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::CreateFromDirectory('%CHIMERA_DIR%', '%CHIMERA_ZIP%', [System.IO.Compression.CompressionLevel]::NoCompression, $false)"
 echo CHIMERA.zip created.
+
+echo.
+echo Clearing region files...
+set "REGION_DIR=C:\MC\MINECRAFT_SERVER_TMP_26-1\world\dimensions\minecraft\c1\region"
+if exist "%REGION_DIR%" (
+    del /Q "%REGION_DIR%\*.*"
+    echo Region files deleted.
+) else (
+    echo Region folder not found, skipping.
+)
 
 echo.
 echo Copy operation completed.
