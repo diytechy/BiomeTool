4e99bbd16e422dd90eec1d4a8f1f15bbcf67585b
Peter Johnson <peterandersjohnson@outlook.com>
2026-04-22 18:13:12 -0500

    Pre subsamplel introduction

--- build.gradle.kts
+++ build.gradle.kts
@@ -127,7 +127,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
 
-    val terraGitHash = "97fe5137d"
+    val terraGitHash = "afd55bddb"
 
     bootstrapTerraAddon("com.dfsek.terra:api-addon-loader:0.1.0-BETA-$terraGitHash")
     bootstrapTerraAddon("com.dfsek.terra:manifest-addon-loader:1.0.0-BETA-$terraGitHash")
