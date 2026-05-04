c27b6790484c17d1f66947bebfefb714680c5f9c
Peter Johnson <peterandersjohnson@outlook.com>
2026-05-04 17:09:26 -0500

    Update to run density sampler

--- build.gradle.kts
+++ build.gradle.kts
@@ -127,7 +127,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
 
-    val terraGitHash = "653aa69c1"
+    val terraGitHash = "3830d10c3"
 
     bootstrapTerraAddon("com.dfsek.terra:api-addon-loader:0.1.0-BETA-$terraGitHash")
     bootstrapTerraAddon("com.dfsek.terra:manifest-addon-loader:1.0.0-BETA-$terraGitHash")
